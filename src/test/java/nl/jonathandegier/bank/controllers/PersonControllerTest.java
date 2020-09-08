c2a525a65009fc6a62973dfddd425a2598085c27
Jonathan de Gier <jonathan.de.gier@outlook.com>
2020-09-08 20:57:49 +0200

    more tests

---
+++ src/test/java/nl/jonathandegier/bank/controllers/PersonControllerTest.java
@@ -0,0 +1,4 @@
+package nl.jonathandegier.bank.controllers;
+
+public class PersonControllerTest {
+}
--- src/test/java/nl/jonathandegier/bank/services/AccountHolderServiceTest.java
+++ src/test/java/nl/jonathandegier/bank/services/AccountHolderServiceTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class AccountHolderTest {
+public class AccountHolderServiceTest {
 
     @Mock
     private AccountRepository accountRepositoryMock;
