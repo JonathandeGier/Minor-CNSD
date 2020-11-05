83e0ec0e9df218212613b495e36f432d30061f58
Jonathan de Gier <jonathan.de.gier@outlook.com>
2020-11-05 12:52:05 +0100

    add secondary index to dynamoDB table

--- notes-app/functions/get_all_notes/lambda_function.py
+++ notes-app/functions/get_all_notes/lambda_function.py
@@ -1,14 +1,15 @@
 import json
 import boto3
 import os
+from boto3.dynamodb.conditions import Key
 
 
-table = boto3.resource('dynamodb').Table(os.environ['TABLENAME'])
+table = boto3.resource("dynamodb").Table(os.environ["TABLENAME"])
 
 
-def lambda_handler(event, context):
+def scan_notes():
     scan_kwargs = {
-        'ProjectionExpression': "id, content, created_at, updated_at"
+        "ProjectionExpression": "id, content, created_at, updated_at"
     }
 
     notes = []
@@ -16,12 +17,31 @@ def lambda_handler(event, context):
     start_key = None
     while not done:
         if start_key:
-            scan_kwargs['ExclusiveStartKey'] = start_key
+            scan_kwargs["ExclusiveStartKey"] = start_key
         response = table.scan(**scan_kwargs)
-        notes.extend(response.get('Items', []))
-        start_key = response.get('LastEvaluatedKey', None)
+        notes.extend(response.get("Items", []))
+        start_key = response.get("LastEvaluatedKey", None)
         done = start_key is None
+    
+    return notes
 
+
+def query_notes_by_content(content):
+    response = table.query(
+        IndexName="ContentIndex",
+        KeyConditionExpression=Key("content").eq(content)
+    )
+    return response["Items"]
+
+
+def lambda_handler(event, context):
+    queryParameters = event["queryStringParameters"]
+    
+    if "content" in queryParameters:
+        notes = query_notes_by_content(queryParameters["content"])
+    else:
+        notes = scan_notes()
+    
     return {
         "statusCode": 200,
         "body": json.dumps(notes)
